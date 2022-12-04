4a845d52a29a2f2ee26be3d800dbcdab8837f295
devKiratu <dev.kiratu@gmail.com>
2022-12-04 13:57:55 +0300

    add desktop hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import heroImage from "../public/img/image-hero-mobile.png";
+import heroImageSmall from "../public/img/image-hero-mobile.png";
+import heroImageLarge from "../public/img/image-hero-desktop.png";
 import audiophile from "../public/img/client-audiophile.svg";
 import databiz from "../public/img/client-databiz.svg";
 import maker from "../public/img/client-maker.svg";
@@ -7,11 +8,16 @@ import meet from "../public/img/client-meet.svg";
 
 export default function Hero() {
   return (
-    <section className="grid grid-cols-1 gap-8 xl:grid-cols-2 xl:gap-24 xl:mx-24 xl:mt-16">
-      <div className="xl:order-2 xl:w-4/5">
-        <Image src={heroImage} alt="" />
+    <section className="grid grid-cols-1 gap-8 xl:grid-cols-2 xl:gap-10 xl:w-4/5 mx-auto xl:mt-10">
+      <div className="xl:order-2">
+        <div className="xl:hidden">
+          <Image src={heroImageSmall} alt="" />
+        </div>
+        <div className="hidden xl:block xl:w-8/12 mx-auto">
+          <Image src={heroImageLarge} alt="" />
+        </div>
       </div>
-      <div className="px-4 text-center flex flex-col justify-center items-center xl:items-start xl:text-left ">
+      <div className="px-4 text-center flex flex-col justify-end items-center xl:items-start xl:text-left xl:ml-8">
         <h1 className="text-lighterBlack text-3xl font-bold xl:text-6xl ">
           Make <span className="xl:block">remote work</span>
         </h1>
@@ -22,7 +28,7 @@ export default function Hero() {
         <button className="px-5 py-3 bg-lighterBlack text-whitish rounded-xl text-xs my-2">
           Learn more
         </button>
-        <div className="flex justify-between items-center my-7  w-full xl:w-4/5 xl:mt-16">
+        <div className="flex justify-between items-center my-7  w-full xl:w-4/5 xl:mt-16 xl:mb-0">
           <Image src={databiz} alt="" width={70} />
           <Image src={audiophile} alt="" width={45} />
           <Image src={meet} alt="" width={60} />
