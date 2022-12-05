809bc19319d5659d51e126d76a8f5404099e9ad2
devKiratu <dev.kiratu@gmail.com>
2022-12-05 13:01:35 +0300

    make dropdown menu accomodate icons where applicable

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -28,15 +28,20 @@ export default function Dropdown({ data }: DropDownProps) {
       </Menu.Button>
       <Menu.Items
         as="div"
-        className="bg-whitish rounded-[10px] p-3 overflow-hidden w-max h-fit absolute top-5 z-50 shadow-[0_0_50px_-12px_rgb(0,0,0,0.25)]"
+        className={`bg-whitish rounded-[10px] p-3 overflow-hidden w-max h-fit absolute top-5 ${
+          data.items[0].icon ? "right-0" : ""
+        } z-50 shadow-[0_0_50px_-12px_rgb(0,0,0,0.25)]`}
       >
         {data.items.map((item) => (
           <Menu.Item key={item.title}>
             {({ active }) => (
               <a
                 href="#"
-                className={`w-full p-2 flex justify-start items-center`}
+                className={`w-full p-2 flex justify-start items-center ${
+                  active ? "text-lighterBlack" : ""
+                }`}
               >
+                {item.icon && <Image src={item.icon} alt="" className="mr-3" />}
                 {item.title}
               </a>
             )}
