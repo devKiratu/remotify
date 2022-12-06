4b62d0a69b4c20cfc7c2751895145a3cc6296882
devKiratu <dev.kiratu@gmail.com>
2022-12-06 11:28:21 +0300

    create stylable wrapper components over svg arrow icons

---
+++ components/icons/ArrowDown.tsx
@@ -0,0 +1,19 @@
+const ArrowDown = () => {
+  return (
+    <svg
+      width="10"
+      height="6"
+      xmlns="http://www.w3.org/2000/svg"
+      className="inline-block ml-2 "
+    >
+      <path
+        stroke="currentColor"
+        stroke-width="1.5"
+        fill="none"
+        d="m1 1 4 4 4-4"
+      />
+    </svg>
+  );
+};
+
+export default ArrowDown;
---
+++ components/icons/ArrowUp.tsx
@@ -0,0 +1,19 @@
+const ArrowUp = () => {
+  return (
+    <svg
+      width="10"
+      height="6"
+      xmlns="http://www.w3.org/2000/svg"
+      className="inline-block ml-2"
+    >
+      <path
+        stroke="currentColor"
+        stroke-width="1.5"
+        fill="none"
+        d="m1 5 4-4 4 4"
+      />
+    </svg>
+  );
+};
+
+export default ArrowUp;
