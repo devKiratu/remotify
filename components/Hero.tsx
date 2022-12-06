fa4bb1d1a20750ac2d17d6f9c4b517c00c6cf7aa
devKiratu <dev.kiratu@gmail.com>
2022-12-06 10:34:18 +0300

    adjust hero responsiveness

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,20 +8,20 @@ import meet from "../public/img/client-meet.svg";
 
 export default function Hero() {
   return (
-    <section className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:gap-10 lg:w-4/5 mx-auto md:mt-6 xl:mt-10">
+    <section className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:gap-10 lg:w-4/5 mx-auto md:mt-10 xl:mt-10">
       <div className="md:order-2">
-        <div className="lg:hidden xs:w-3/4 sm:w-3/5 md:w-4/5 m-auto">
+        <div className="md:hidden xs:w-3/4 sm:w-3/5 m-auto">
           <Image src={heroImageSmall} alt="" />
         </div>
-        <div className="hidden lg:block lg:w-2/3 xl:w-8/12 mx-auto">
+        <div className="hidden md:block w-2/3 xl:w-8/12 mx-auto">
           <Image src={heroImageLarge} alt="" />
         </div>
       </div>
       <div className="px-4 md:px-2 md:pr-0 lg:px-4 text-center flex flex-col justify-end items-center xs:w-4/5 xs:mx-auto md:items-start md:text-left lg:w-full xl:ml-6">
-        <h1 className="text-lighterBlack text-3xl font-bold xl:text-6xl ">
+        <h1 className="text-lighterBlack text-3xl font-bold md:text-4xl xl:text-6xl ">
           Make <span className="md:block">remote work</span>
         </h1>
-        <p className="text-sm lg:text-sm my-3 md:text-xs md:my-4 xl:my-10 xl:w-4/5">
+        <p className="text-sm my-3 md:my-8 xl:my-10 xl:w-4/5">
           Get your team in sync, no matter your location. Streamline processes,
           create team rituals, and watch productivity soar.
         </p>
